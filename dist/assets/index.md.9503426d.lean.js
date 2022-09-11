ffff6b1dd2c83039f6369a1ad31335fe3c31c668
Tommy Vo <votommy@outlook.com>
2022-09-11 13:20:23 -0500

    Initial commit

---
+++ dist/assets/index.md.9503426d.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o,b as a}from"./app.5ca8fdfc.js";const h=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"index.md"}'),n={name:"index.md"},s=a("h1",null,"Hi, I'm Tommy",-1),r=[s];function c(d,i,_,m,p,l){return o(),t("div",null,r)}const u=e(n,[["render",c]]);export{h as __pageData,u as default};
