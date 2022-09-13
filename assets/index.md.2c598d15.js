9958d2248736d17abb509c1b7a789b16f5e41884
Tommy Vo <votommy@outlook.com>
2022-09-12 21:03:51 -0500

    Fix logo

---
+++ assets/index.md.2c598d15.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o,d as a}from"./app.c78327a5.js";const h=JSON.parse('{"title":"","description":"","frontmatter":{},"headers":[],"relativePath":"index.md"}'),n={name:"index.md"},s=a("h1",null,"Hi, I'm Tommy",-1),r=[s];function c(d,i,_,m,p,l){return o(),t("div",null,r)}const u=e(n,[["render",c]]);export{h as __pageData,u as default};
